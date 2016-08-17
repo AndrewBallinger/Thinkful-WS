90e0723784b56926049f2bd6611169b90f8451e9
Andrew Ballinger <aballinger@payjunction.com>
2016-08-17 13:22:32 -0700

    Rearchitecting completed for routes

--- waitstaffApp.js
+++ waitstaffApp.js
@@ -1,67 +1,72 @@
-angular.module('WaitstaffApp', [])
-  .controller('WaitstaffCtrl', function($scope){
-     var ws = this;
-      
-     ws.price = 0;
-     ws.tip = 0;
-     ws.tax = 0;
-     ws.meals = [];
+angular.module('WaitstaffApp', ['ngRoute'])
+       .value('meals', [])
+       .config(['$routeProvider', ($routeProvider) => {
+         $routeProvider
+           .when('/', {
+             templateUrl : 'home.html' })
+           .when('/new-meal', {
+             templateUrl : 'new-meal.html',
+             controller : 'WaitstaffCtrl as waitstaff'})
+           .when('/my-earnings', {
+             templateUrl : 'my-earnings.html',
+             controller : 'WaitstaffCtrl as waitstaff' })
+       }])
+       .controller('WaitstaffCtrl', function($scope, meals){
+         var waitstaff = this;
+         
+         waitstaff.price = 0;
+         waitstaff.tip = 0;
+         waitstaff.tax = 0;
+         waitstaff.last_tip = 0;
+         waitstaff.last_subtotal = 0;
+         waitstaff.last_total = 0;
+         
+         waitstaff.submit = function() {
+           var subtotal = waitstaff.roundCurrency( waitstaff.price + (waitstaff.price*(waitstaff.tax/100)) );
+           var tip = waitstaff.roundCurrency( (waitstaff.tip/100)*subtotal );
+           var total = subtotal + tip;
 
-     ws.submit = function() {
-       var subtotal = ws.roundCurrency( ws.price + (ws.price*(ws.tax/100)) );
-       var tip = ws.roundCurrency( (ws.tip/100)*subtotal );
-       var total = subtotal + tip;
+           meals.push({
+             subtotal: subtotal,
+             tip: tip,
+             total: total
+           });
 
-       ws.meals.push({
-         subtotal: subtotal,
-         tip: tip,
-         total: total
+           waitstaff.clear();
+           waitstaff.last_tip = tip;
+           waitstaff.last_total = total;
+           waitstaff.last_subtotal = subtotal;
+         } 
+
+         waitstaff.clear = function() {
+           waitstaff.price = 0;
+           waitstaff.tax = 0;
+           waitstaff.tip = 0;
+           waitstaff.last_tip = 0;
+           waitstaff.last_subtotal = 0;
+           waitstaff.last_total = 0;
+         } 
+
+         waitstaff.reset = function() {
+           waitstaff.clear();
+           meals = [];
+         } 
+
+         waitstaff.tip_total = function() {
+           if (meals.length == 0) return 0;
+           return meals.reduce( (acc, meal) => acc + meal.tip , 0 )
+         }
+
+         waitstaff.meal_count = function() {
+           return meals.length;
+         }
+
+         waitstaff.tpm = function() {
+           if (meals.length == 0) return 0;
+           return waitstaff.tip_total() / waitstaff.meal_count();
+         }
+
+         waitstaff.roundCurrency = function(amount) {
+	       return Math.round(amount * 100) / 100;
+         }
        });
-
-       ws.clear();
-     } 
-
-     ws.clear = function() {
-       ws.price = 0;
-       ws.tax = 0;
-       ws.tip = 0;
-     } 
-
-     ws.reset = function() {
-      ws.clear();
-      ws.meals = [];
-     } 
-
-     ws.subtotal = function() {
-       if (ws.meals.length == 0) return 0;
-       return ws.meals[ws.meals.length-1].subtotal;
-     }
-
-     ws.last_tip = function() {
-       if (ws.meals.length == 0) return 0;
-       return ws.meals[ws.meals.length-1].tip;
-     }
-
-    ws.total = function() {
-      if (ws.meals.length == 0) return 0;
-      return ws.meals[ws.meals.length-1].total;
-    }
-
-    ws.tip_total = function() {
-      if (ws.meals.length == 0) return 0;
-      return ws.meals.reduce( (acc, meal) => acc + meal.tip , 0 )
-    }
-
-    ws.meal_count = function() {
-      return ws.meals.length;
-    }
-
-    ws.tpm = function() {
-      if (ws.meals.length == 0) return 0;
-      return ws.tip_total() / ws.meal_count();
-    }
-
-    ws.roundCurrency = function(amount) {
-	return Math.round(amount * 100) / 100;
-    }
-  });
